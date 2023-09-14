ee9c9c267be66e73337ce054048fa943a818be8a
nandankumarms <nandankumarms31@gmail.com>
2023-09-14 23:16:20 +0530

    source code changed

--- Application_SourceCode/src/main/java/Login/LoginClass.java
+++ Application_SourceCode/src/main/java/Login/LoginClass.java
@@ -3,7 +3,7 @@ package Login;
 public class LoginClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Login successfull");
+		System.out.println("Did some changes in the code");
 	}
 
 }
